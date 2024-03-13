f603ab54ed768f341629b6e75556405dada26cff
whoru22 ra <j1u2n3h4o7@gmail.com>
2024-03-13 02:22:37 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,7 @@ APPNAME="classify_whoru"
 ENV_FILE=".env"
 PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
 
-snarkos developer execute "${APPNAME}.aleo"  main \
-e0:0u32 \
-e1:0u32 \ 
-  --path "./build/" \
+snarkos developer execute "${APPNAME}.aleo"  main '{e0:0u32, e1:0u32}' \
   --private-key "${PRIVATE_KEY}" \
   --query "https://api.explorer.aleo.org/v1" \
   --priority-fee 3000000 \
