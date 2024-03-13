52999cc2f548546a1042d756769d7bed0ab66fe1
whoru22 ra <j1u2n3h4o7@gmail.com>
2024-03-13 02:18:19 +0000

    add aleo zkml

---
+++ run.sh
@@ -0,0 +1,12 @@
+APPNAME="classify_whoru"
+ENV_FILE=".env"
+PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+
+snarkos developer execute "${APPNAME}.aleo"  main \
+e0:0u32 \
+e1:0u32 \ 
+  --path "./build/" \
+  --private-key "${PRIVATE_KEY}" \
+  --query "https://api.explorer.aleo.org/v1" \
+  --priority-fee 3000000 \
+  --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" \
